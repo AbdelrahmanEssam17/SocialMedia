4d709aca70bcaca23b36d0bfbed82c2e596e9030
”abdelrahman <eabdo6990@gmail.com>
2026-06-03 01:31:38 +0300

    register ui

--- social-media/src/Pages/Register.jsx
+++ social-media/src/Pages/Register.jsx
@@ -2,8 +2,25 @@ import React from "react";
 
 export default function Register() {
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <div className="bg-white rounded-2xl shadow-2xl py-10 px-6 min-w-md ">
+        <h1 className="flex justify-center text-2xl mb-4">Register</h1>
+        <form className="flex flex-col gap-4">
+          <input label="name" type="text" name="name" placeholder="name" />
+          <input label="email" type="name" placeholder="email" />
+          <input label="password" type="password" placeholder="password" />
+          <input label="password" type="password" placeholder="Repassword" />
+          <div className="flex ">
+            <input label="date" type="date" />
+            <select name="gender" id="gender">
+              <option value="">Select your gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
+          <button className="bg-amber-100 hover:bg-amber-700">Register</button>
+        </form>
+      </div>
+    </>
   );
 }
